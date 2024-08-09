344f01e7e9868ed5cf024b7cbd48ba65d7e576e4
Kirtan Parikh <119340062+Kirtan134@users.noreply.github.com>
2024-08-09 23:39:44 +0530

    Time: 216 ms (18.19%) | Memory: 89.3 MB (14.03%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>> triples;
-        for(auto it: points){
-            triples.push_back({it[0]*it[0]+ it[1]*it[1], it[0],it[1]});
-        }
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq(triples.begin(), triples.end());
         vector<vector<int>> ans;
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        for(auto it: points){
+            pq.push({it[0]*it[0]+ it[1]*it[1], it[0],it[1]});
+        }
         while(k--){
             vector<int> el = pq.top(); pq.pop();
             ans.push_back({el[1],el[2]});
