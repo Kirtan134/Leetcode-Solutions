c514701abf35cab60c676909ef34b276e374d300
Kirtan Parikh <119340062+Kirtan134@users.noreply.github.com>
2024-08-11 06:43:10 +0530

    Time: 3 ms (47.35%) | Memory: 7.5 MB (60.44%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size()!=goal.size()) return false;
-        string combined = s + s;
-        return combined.find(goal) != string::npos;
+        if (s.size() != goal.size()) return false;
+        int n = s.size();
+        
+        for (int i = 0; i < n; i++) {
+            bool isMatch = true;
+            for (int j = 0; j < n; j++) {
+                if (s[j] != goal[(i + j) % n]) {
+                    isMatch = false;
+                    break;
+                }
+            }
+            if (isMatch) return true;
+        }
+        
+        return false;
     }
 };
