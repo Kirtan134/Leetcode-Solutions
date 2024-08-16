ddfaa1ddcfedb62322c9d2b8c0fe7c6d29e76055
Kirtan Parikh <119340062+Kirtan134@users.noreply.github.com>
2024-08-16 21:58:46 +0530

    Time: 4 ms (79.46%) | Memory: 12.2 MB (43.02%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,11 +1,17 @@
+// Most Optimal Solution
+// Time: O(n)
+// Space: O(1) 
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i+=3){
-            if(nums[i]!=nums[i-1]) return nums[i-1];
+        int ones=0, twos=0;
+        for(int i=0;i<nums.size();i++){
+            // add in ones if not in twos
+            ones = (ones^nums[i]) & ~twos;
+            // add in twos if not in ones
+            twos = (twos^nums[i]) & ~ones;
+            // add in threes if not in twos (not needed)
         }
-        return nums[n-1];
+        return ones;
     }
 };
